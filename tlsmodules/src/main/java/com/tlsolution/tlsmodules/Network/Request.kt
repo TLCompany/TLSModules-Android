bbdfdf5db00525d4c710d00da9b4a4c9d6f1fa5f
RyanBang1209 <seokk1209@gmail.com>
2020-03-02 11:43:19 +0900

    modules updated

--- tlsmodules/src/main/java/com/tlsolution/tlsmodules/Network/Request.kt
+++ tlsmodules/src/main/java/com/tlsolution/tlsmodules/Network/Request.kt
@@ -2,9 +2,12 @@ package com.tlsolution.tlsmodules.Network
 
 interface Request {
     var baseURL: String
-    var url: String
+    var url: String?
     var body: String?
-    var method: String
-    var endpoint: String?
+    var method: String?
+    var endpoint: String
     var tokenURL: String
+    var environmentMode: RequestEnvironmentMode
+
+    fun getBaseURLString(): String
 }
